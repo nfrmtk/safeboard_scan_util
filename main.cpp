043f73e05d9ca7172f108de2693b463d7ee6c228
nfrmtk <Lykov.ivan.02@mail.ru>
2022-06-11 22:08:15 +0300

    I/O corrections.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,21 @@
 #include <scan_lib.h>
 #include <argparse.hpp>
 
+std::string make_working_directory(const std::vector<std::string>& directory_vector){
+    std::string ans = "";
+    for (const auto& str: directory_vector) {
+        ans+= (str + " ");
+    }
+    return ans.substr(0, ans.size() - 1);
+}
+
+void output_time(const std::time_t& time){
+    uint16_t ms = time%1000, seconds = (time/1000)%60, minutes = time/60000;
+    std::cout << std::setw(20) << "Executaion time: ";
+    if (ms) std::cout << ms << "ms";
+    if (seconds) std::cout <<  ", " <<seconds << "s ";
+    if (minutes) std::cout << ", " <<minutes << "min\n";
+}
 
 void output_result(const scan_util::info& results){
     std::cout << "\n\nscan_util have finished working. Here are the results:\n";
@@ -12,13 +27,14 @@ void output_result(const scan_util::info& results){
     std::cout << std::setw(20) <<"CMD detects:   " << results.numbers.cmd_detects << '\n';
     std::cout << std::setw(20) <<"EXE detects:   " << results.numbers.exe_detects << '\n';
     std::cout << std::setw(20) <<"Errors:   " << results.numbers.errors << '\n';
-    std::cout << std::setw(20) << "Executaion time:   " << results.time_for_scanning << "ms\n";
+    output_time(results.time_for_scanning);
 }
+
 
 int main(int argc, char* argv[]) {
 
     argparse::ArgumentParser ap("scan_util", "1.0");
-    ap.add_argument("directory");
+    ap.add_argument("directory").remaining();
 
     try{
         ap.parse_args(argc, argv);
@@ -29,7 +45,8 @@ int main(int argc, char* argv[]) {
     }
 
     try{
-        auto working_directory = ap.get<std::string>("directory");
+        auto working_directory_vector = ap.get<std::vector<std::string>>("directory");
+        std::string working_directory = make_working_directory(working_directory_vector);
         scan_util scanUtil(working_directory);
         auto scan_info = scanUtil.scan();
         output_result(scan_info);
@@ -38,5 +55,6 @@ int main(int argc, char* argv[]) {
         std::cerr << err.what() << "occured. The directory is incorrect\n";
         std::cerr << ap;
     }
+
     return 0;
 }
